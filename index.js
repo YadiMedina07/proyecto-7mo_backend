b3aa467d831126e68d878a414f2c91a84bffa7f2
Yadi-Medina <Yadi-Medina@example.com>
2024-10-24 12:38:17 -0600

    sepa2

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import './src/database.js';      // Importación de database.js desde src/
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(Server started on port ${PORT});
+  console.log(`Server started on port ${PORT}`); // Usa backticks para interpolación
 });
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './database.js';      // Importación de database.js desde src/
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Server started on port ${PORT}`); // Usa backticks para interpolación
 });
